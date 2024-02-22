5533f16c36fb897fd987a614846d4a47da16b43c
otlevire <eriveltoclenio@gmail.com>
2024-02-22 10:21:15 -0800

    feat: interface in chat pdf

--- pages/Chat_com_pdf.py
+++ pages/Chat_com_pdf.py
@@ -8,6 +8,8 @@ from utils import create_docs, split_docs, get_embeddings, get_vectorstore
 from langchain.chains.conversation.memory import ConversationSummaryMemory
 load_dotenv()
 
+
+
 if 'chain' not in st.session_state:
     st.session_state['chain'] = None
 if 'chat_history' not in st.session_state:
@@ -18,12 +20,16 @@ if 'db' not in st.session_state:
     st.session_state['db'] = None
 if 'unique_id' not in st.session_state:
     st.session_state['unique_id'] =''
+if 'is_ready' not in st.session_state:
+    st.session_state['is_ready'] =False
 st.session_state['API_KEY'] =  os.getenv("OPENAI_API_KEY")
 
 st.set_page_config(page_title="Cicero - Assistente Documental da PCA", page_icon=":robot_face:")
 st.header("Faça o upload dos documentos pdf 📄")
 pdfs_uploaded = st.file_uploader("Carregue os seus PDFs aqui e clica em 'Processar'", accept_multiple_files=True, type=["pdf"])
-if st.button("Processar"):
+btn_upload = st.button("Processar")
+
+if btn_upload:
     with st.spinner("Processando..."):    
         st.session_state['unique_id'] = uuid.uuid4().hex
         # get pdf text
@@ -47,16 +53,17 @@ if st.button("Processar"):
         st.session_state['chain'] = get_chain(st.session_state['API_KEY'])
         st.success("Chain criado✅")
 
+        st.session_state['is_ready']= True
 
-response_container = st.container()
-container = st.container()
 
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        question = st.text_area(label="Digite a sua pergunta:",key='question_input', height=100)
-        submit_button = st.form_submit_button(label='Enviar')
 
-        if submit_button:
+if st.session_state['is_ready']:
+    response_container = st.container()
+    container = st.container()
+
+    with container:
+        question = st.chat_input("Digite a sua pergunta:")
+        if question:
             st.session_state['chat_history'].append(question)
             relevant_docs = get_similar_docs(db=st.session_state['db'], query=question, k=1)
             print(f"{'='*200}\nDocumentos Base da resposta:{relevant_docs}")
@@ -66,8 +73,10 @@ with container:
             with response_container:
                 for i in range(len(st.session_state['chat_history'])):
                         if (i % 2) == 0:
-                            message(st.session_state['chat_history'][i], is_user=True, key=str(i) + '_user')
+                            with st.chat_message("user"):
+                                st.write(st.session_state['chat_history'][i])
                         else:
-                            message(st.session_state['chat_history'][i], key=str(i) + '_AI')
+                            with st.chat_message("assistant"):
+                                st.write(st.session_state['chat_history'][i])
         
     
